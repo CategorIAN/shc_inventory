c962d8ea5db9ce9304c5d6e1e2a63e37d7b7615a
CategorIAN <ianholm9@gmail.com>
2024-08-26 11:57:41 -0600

    ..

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,20 +3,27 @@ from django.urls import include, path
 from . import views
 
 app_name = "polls"
-urlpatterns = []
 
 urlpatterns = [
     #======Index=============================
     path("", views.index, name="index"),
     #=====Forms==============================
     path("simplepurchase/", views.simple_purchase, name="simplepurchase"),
-    path("purchase_initial/", views.purchase_initial, name="purchase_initial"),
-    path("purchase_items/<employee>/<dept>/", views.purchase_items, name="purchase_items"),
     #====Actions=============================
     path("do_nothing/", views.do_nothing, name="do_nothing"),
     path("modelform_to_model/", views.modelform_to_model, name="modelform_to_model"),
-    path("enter_initial/", views.enter_initial, name="enter_initial"),
-    path("submit_purchase/<id>/", views.submit_purchase, name="submit_purchase"),
     #=====Submission Response=================
     path("thanks/", views.thanks, name="thanks"),
+] + [
+    path("purchase_contact/", views.purchase_contact, name="purchase_contact"),
+    path("purchase_contact_get/", views.purchase_contact_get, name="purchase_contact_get"),
+    path("purchase_items/<employee>/<dept>/", views.purchase_items, name="purchase_items"),
+    path("purchase_save/", views.purchase_save, name="purchase_save")
+] + [
+    path("cart_contact/", views.cart_contact, name="cart_contact"),
+    path("cart_contact_get/", views.cart_contact_get, name="cart_contact_get"),
+    path("cart_items/<employee>/<dept>/", views.cart_items, name="cart_items"),
+    path("cart_save/<employee>/<dept>/", views.cart_save, name="cart_save")
 ]
+
+#A purchase has purchase_contact, purchase_contact_get, purchase_items, purchase_save, and thanks.
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
-from .models import SimplePurchaseForm, PurchaseForm, Employee, Department, Item_Purchase, Purchase, getItemPurchaseForm
+from .models import (SimplePurchaseForm, PurchaseForm, Employee, Department, Item_Purchase, Purchase,
+                     Item_Purchase_Form, getItemPurchaseForm)
 from django.shortcuts import render
 from django.forms import modelformset_factory
 
@@ -11,18 +12,18 @@ def thanks(request):
 
 def do_nothing(request):
     return HttpResponseRedirect("/polls/thanks")
-
+#==================================================================================================================
 def simple_purchase(request):
     return render(request, "polls/simplepurchase.html", {"form": SimplePurchaseForm()})
 
 def modelform_to_model(request):
     SimplePurchaseForm(request.POST).save()
     return HttpResponseRedirect("/polls/thanks")
+#==================================================================================================================
+def purchase_contact(request):
+    return render(request, "polls/purchase_contact.html", {"form": PurchaseForm()})
 
-def purchase_initial(request):
-    return render(request, "polls/purchase_initial.html", {"form": PurchaseForm()})
-
-def enter_initial(request):
+def purchase_contact_get(request):
     initial = PurchaseForm(request.POST)
     employee, dept = initial.data['employee'], initial.data['dept']
     return HttpResponseRedirect("/polls/purchase_items/{}/{}".format(employee, dept))
@@ -35,14 +36,47 @@ def purchase_items(request, employee, dept):
     form = getItemPurchaseForm(purchase.pk)
     IPFormSet = modelformset_factory(Item_Purchase, form=form, min_num=2)
     formset = IPFormSet(queryset=Item_Purchase.objects.none())
-    context = {"id": purchase.pk, "employee": employee, "dept": dept, "formset": formset}
+    context = {"employee": employee, "dept": dept, "formset": formset}
     return render(request, "polls/purchase_items.html", context)
 
-def submit_purchase(request, id):
-    form = getItemPurchaseForm(id)
-    RIPFormSet = modelformset_factory(Item_Purchase, form=form)
+def purchase_save(request):
+    RIPFormSet = modelformset_factory(Item_Purchase, form=Item_Purchase_Form)
     formset = RIPFormSet(request.POST)
     formset.save()
     return HttpResponseRedirect("/polls/thanks")
+#==================================================================================================================
+def cart_contact(request):
+    return render(request, "polls/cart_contact.html", {"form": PurchaseForm()})
 
+def cart_contact_get(request):
+    initial = PurchaseForm(request.POST)
+    employee, dept = initial.data['employee'], initial.data['dept']
+    return HttpResponseRedirect("/polls/cart_items/{}/{}".format(employee, dept))
+
+def cart_items(request, employee, dept):
+    employee_obj = Employee.objects.get(id=employee)
+    dept_obj = Department.objects.get(cost_center=dept)
+    purchase = Purchase(employee=employee_obj, dept=dept_obj)
+    purchase.save()
+    form = getItemPurchaseForm(purchase.pk)
+    IPFormSet = modelformset_factory(Item_Purchase, form=form, max_num=2)
+    formset_1 = IPFormSet(queryset=Item_Purchase.objects.none(), prefix="formset1")
+    formset_2 = IPFormSet(queryset=Item_Purchase.objects.none(), prefix="formset2")
+    context = {"employee": employee, "dept": dept, "formset_1": formset_1, "formset_2": formset_2}
+    return render(request, "polls/results.html", context)
+
+def cart_save(request, employee, dept):
+    RIPFormSet = modelformset_factory(Item_Purchase, form=Item_Purchase_Form)
+    if "add_item" in request.POST:
+        formset_1 = RIPFormSet(request.POST, prefix="formset1")
+        formset_2 = RIPFormSet(request.POST, prefix="formset2")
+        current_cart = list(formset_1) + list(formset_2)
+        context = {"employee": employee, "dept": dept, "formset_1": formset_1,
+                   "formset_2": current_cart}
+        return render(request, "polls/cart_items.html", context)
+    elif "checkout" in request.POST:
+        formset = RIPFormSet(request.POST, prefix="formset2")
+        formset.save()
+    return HttpResponseRedirect("/polls/thanks")
+#==================================================================================================================
 
